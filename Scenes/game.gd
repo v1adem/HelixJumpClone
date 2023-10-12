a7276f9fe5140441290132b5564e1177e0f833f5
MihaVar <mykhailvarych@gmail.com>
2023-10-12 21:27:59 +0300

    made 1st level

---
+++ Scenes/game.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("left"):
+		$test_lvl.rotate_y(0.05)
+	elif Input.is_action_pressed("right"):
+		$test_lvl.rotate_y(-0.05)
+	
