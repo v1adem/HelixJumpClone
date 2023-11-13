a7f48362f60d4442b407fa2c29ee9e684ac46d5d
MihaVar <mykhailvarych@gmail.com>
2023-11-13 11:26:17 +0200

    made pillar white and fixed first platform bug

--- Scripts/test_lvl.gd
+++ Scripts/test_lvl.gd
@@ -7,8 +7,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("left"):
-		$platform.rotate_y(0.05)
-	elif Input.is_action_pressed("right"):
-		$platform.rotate_y(-0.05)
+	pass
 	
